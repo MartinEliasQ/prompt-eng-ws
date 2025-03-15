0ae0d905bebece689447e817cad6ae804fa12404
MartinEliasQ <elias@guane.com.co>
2025-03-15 08:41:31 -0500

    first commit

---
+++ reto001/quote.py
@@ -0,0 +1,42 @@
+quote001 = """
+
+
+
+
+Buen Día , Equipo.
+
+
+Agradezco su ayuda confirmando tarifas de acuerdo con la siguiente información:,
+
+
+Origin: Udea Freigth Corp.
+
+Destination: Ft. Pierce, FL 34982
+
+Commodity: General
+
+Stackable: Si
+
+Weight: 11132 Lbs total
+
+Pieces: 8 Pallets
+
+Dimensions: 40 X 48 X 83 (in)
+
+
+Origin: Udea Freigth Corp.
+
+Destination: Ft. Pierce, FL 34982
+
+Commodity: General
+
+Stackable: Si
+
+Weight: 4842 Lbs total
+
+Pieces: 9 Pallets
+
+Dimensions: 40 X 48 X 83 (in)
+
+
+Quedo atenta, muchas gracias ."""
